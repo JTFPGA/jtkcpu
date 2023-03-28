e370f9fff74321edb1e329584c399364f50142b4
JOTEGO <jotego@gmail.com>
2023-03-28 14:01:36 +0200

    Admit ASPATH to use as the path for the ASL assembler

--- ver/top/sim.sh
+++ ver/top/sim.sh
@@ -2,13 +2,17 @@
 
 TEST=$1
 
+if [ ! -z "$ASPATH" ]; then
+    ASPATH=`which asl`
+fi
+
 if [ ! -e "tests/$TEST".asm ]; then
     echo "Cannot find $TEST.asm in tests"
     exit 1
 fi
 
-asl -cpu 6809 tests/$TEST.asm > asl.log || (cat asl.log; exit 1)
-p2bin tests/$TEST.p >> asl.log || ( cat asl.log; exit 1)
+$ASPATH/asl -cpu 6809 tests/$TEST.asm > asl.log || (cat asl.log; exit 1)
+$ASPATH/p2bin tests/$TEST.p >> asl.log || ( cat asl.log; exit 1)
 mv tests/$TEST.bin test.bin
 rm -f tests/$TEST.p
 
