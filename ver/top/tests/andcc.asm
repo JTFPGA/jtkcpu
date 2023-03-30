b8f423f8647f961d97e02b286f65f45d5630cc08
flor <floryana.gonzalez@jotego.es>
2023-03-30 17:14:12 +0200

    Debugging andcc, orcc

---
+++ ver/top/tests/andcc.asm
@@ -0,0 +1,42 @@
+; Load test
+; 0000-0FFF RAM
+; 1000      Simulation control
+; 1001		read only, A[23:16]
+; F000-FFFF ROM
+
+; Simulation control bits
+; 0 -> set to finish the sim
+; 1 -> set to mark a bad result
+; 5 -> set to trigger IRQ.  Clear manually
+; 6 -> set to trigger FIRQ. Clear manually
+; 7 -> set to trigger NMI.  Clear manually
+
+TESTCTRL EQU $1000
+
+        ORG $F000
+RESET:  LDA #$FD
+        ANDCC #$F4
+        LDB #$F2
+        ADDB #$88
+        ANDCC #$02
+        BCS BAD
+        BVC BAD
+        LDA #$01
+        ANDCC #$FD
+        BLE BAD
+
+END:    LDX #$BABE
+        LDA #1
+        LDX #TESTCTRL
+        STA ,X                  ; Finish test, result ok
+        BRA END
+BAD:    LDX #$DEAD
+        LDA #3
+        LDX #TESTCTRL
+        STA ,X                  ; Finish test, result bad
+        BRA BAD
+
+; fill with zeros... up to interrupt table
+        DC.B  [$FFFE-*]0
+        FDB   RESET
+
---
+++ ver/top/tests/orcc.asm
@@ -0,0 +1,41 @@
+; Load test
+; 0000-0FFF RAM
+; 1000      Simulation control
+; 1001		read only, A[23:16]
+; F000-FFFF ROM
+
+; Simulation control bits
+; 0 -> set to finish the sim
+; 1 -> set to mark a bad result
+; 5 -> set to trigger IRQ.  Clear manually
+; 6 -> set to trigger FIRQ. Clear manually
+; 7 -> set to trigger NMI.  Clear manually
+
+TESTCTRL EQU $1000
+
+        ORG $F000
+RESET:  LDB #$88
+        ORCC #$04
+        BNE BAD
+        LDA #$07
+        ORCC #$0E
+        BVC BAD
+        LDB #02
+        ORCC #01
+        BCC BAD
+
+END:    LDX #$BABE
+        LDA #1
+        LDX #TESTCTRL
+        STA ,X                  ; Finish test, result ok
+        BRA END
+BAD:    LDX #$DEAD
+        LDA #3
+        LDX #TESTCTRL
+        STA ,X                  ; Finish test, result bad
+        BRA BAD
+
+; fill with zeros... up to interrupt table
+        DC.B  [$FFFE-*]0
+        FDB   RESET
+
