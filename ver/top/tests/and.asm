f347a800285e26aab0b0d67022059c56088e7358
flor <floryana.gonzalez@jotego.es>
2023-03-30 16:07:06 +0200

    And, or, eor, bit pass

---
+++ ver/top/tests/and.asm
@@ -0,0 +1,40 @@
+; Load test
+; 0000-0FFF RAM
+; 1000      Simulation control
+; 1001		read only, A[23:16]
+; F000-FFFF ROM
+
+; Simulation control bits
+; 0 -> set to finish the sim
+; 1 -> set to mark a bad result
+; 5 -> set to trigger IRQ.  Clear manually
+; 6 -> set to trigger FIRQ. Clear manually
+; 7 -> set to trigger NMI.  Clear manually
+
+TESTCTRL EQU $1000
+
+        ORG $F000
+RESET:  LDA #$57
+        ANDA #$0F
+        CMPA #$07
+        BNE BAD
+        LDB #$3B
+        BITB #$6B
+        CMPB #$2B
+        BNE BAD
+
+END:    LDX #$BABE
+        LDA #1
+        LDX #TESTCTRL
+        STA ,X                  ; Finish test, result ok
+        BRA END
+BAD:    LDX #$DEAD
+        LDA #3
+        LDX #TESTCTRL
+        STA ,X                  ; Finish test, result bad
+        BRA BAD
+
+; fill with zeros... up to interrupt table
+        DC.B  [$FFFE-*]0
+        FDB   RESET
+
---
+++ ver/top/tests/eor.asm
@@ -0,0 +1,40 @@
+; Load test
+; 0000-0FFF RAM
+; 1000      Simulation control
+; 1001		read only, A[23:16]
+; F000-FFFF ROM
+
+; Simulation control bits
+; 0 -> set to finish the sim
+; 1 -> set to mark a bad result
+; 5 -> set to trigger IRQ.  Clear manually
+; 6 -> set to trigger FIRQ. Clear manually
+; 7 -> set to trigger NMI.  Clear manually
+
+TESTCTRL EQU $1000
+
+        ORG $F000
+RESET:  LDB #$8F
+        LDA #$09
+        EORB #$F0
+        EORA #$C8
+        CMPA #$C1
+        BNE BAD
+        CMPB #$7F
+        BNE BAD
+
+END:    LDX #$BABE
+        LDA #1
+        LDX #TESTCTRL
+        STA ,X                  ; Finish test, result ok
+        BRA END
+BAD:    LDX #$DEAD
+        LDA #3
+        LDX #TESTCTRL
+        STA ,X                  ; Finish test, result bad
+        BRA BAD
+
+; fill with zeros... up to interrupt table
+        DC.B  [$FFFE-*]0
+        FDB   RESET
+
