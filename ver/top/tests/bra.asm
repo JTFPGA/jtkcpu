e959619e356b53e7d6f2f454962203e46c2d938e
Jose Tejada <jotego@gmail.com>
2023-04-01 06:12:09 +0200

    BRA/LBRA/NOP tested

---
+++ ver/top/tests/bra.asm
@@ -0,0 +1,42 @@
+; Load test
+; 0000-0FFF RAM
+; 1000      Simulation control
+; 1001		read only, A[23:16]
+; F000-FFFF ROM
+
+; Simulation control bits
+; 0 -> set to finish the sim
+; 1 -> set to mark a bad result
+; 5 -> set to trigger IRQ.  Clear manually
+; 6 -> set to trigger FIRQ. Clear manually
+; 7 -> set to trigger NMI.  Clear manually
+
+TESTCTRL EQU $1000
+DATAW    EQU $0100
+
+        ORG $F000
+RESET:  NOP
+        BRA SHORT
+        DC.B    [$23]0
+        LBRA BAD
+SHORT:  NOP
+        LBRA END
+        DC.B    [$230]0
+        BRA BAD
+
+END:    LDU #$BABE
+        LDA #1
+        LDS #TESTCTRL
+        STA ,S                  ; Finish test, result ok
+        BRA END
+
+BAD:    LDU #$DEAD
+        LDA #3
+        LDS #TESTCTRL
+        STA ,S                  ; Finish test, result bad
+        BRA BAD
+
+DATAR:  FDB   $1234, $5678, $9ABC, $DEF0
+
+        DC.B  [$FFFE-*]0
+        FDB   RESET
