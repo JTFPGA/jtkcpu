ecba7f12075c2403838b4e0001da8510e2bb79e2
flor <floryana.gonzalez@jotego.es>
2023-04-04 17:04:16 +0200

    puls/pulu pass

---
+++ ver/top/tests/puls.asm
@@ -0,0 +1,56 @@
+; Load test
+; 0000-0FFF RAM
+; 1000      Simulation control
+; 1001		read only, A[23:16]
+; F000-FFFF ROM
+
+; Simulation control bits
+; 0 -> set to finish the sim
+; 1 -> set to mark a bad result
+; 5 -> set to trigger IRQ.  Clear manually
+; 6 -> set to trigger FIRQ. Clear manually
+; 7 -> set to trigger NMI.  Clear manually
+
+RAMEND   EQU $1000
+TESTCTRL EQU $1000
+DATAW    EQU $0100
+
+        ORG $F000
+RESET:  LDS #RAMEND
+        CLRB
+        LDA #$23
+        PSHS A                  ; PUSH A
+
+        LDB ,S
+        CMPB #$23
+        BNE BAD
+        ADDB #01
+
+        CLRA
+        PSHS B                  ; PUSH B
+        PULS A
+        CMPA #$24
+        BNE BAD
+        CLRB
+        PULS B
+
+        LDD #$1234              ; PUSH D
+        PSHS D
+        CLRD
+        CMPD #0
+        BNE BAD
+        LDD ,S
+        CMPD #$1234
+        BNE BAD
+        CLRD
+        PULS B
+        PULS A
+        CMPD #$3412
+        BNE BAD
+
+        include finish.inc
+
+DATAR:  FDB   END,$1234, $5678, $9ABC, $DEF0
+
+        DC.B  [$FFFE-*]0
+        FDB   RESET
