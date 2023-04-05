3388ee571f1529e308f3387eaf305794d483aead
Jose Tejada <jotego@gmail.com>
2023-04-05 06:42:40 +0200

    clrw PASS

---
+++ ver/top/tests/clrw.asm
@@ -0,0 +1,41 @@
+; Load test
+; 0000-0FFF RAM
+; 1000      Simulation control
+; 1001		read only, A[23:16]
+; F000-FFFF ROM
+
+; Simulation control bits
+; 0 -> set to finish the sim
+; 1 -> set to mark a bad result
+; 5 -> set to trigger IRQ.  Clear manually
+; 6 -> set to trigger FIRQ. Clear manually
+; 7 -> set to trigger NMI.  Clear manually
+
+RAMEND   EQU $1000
+TESTCTRL EQU $1000
+
+        ORG $F000
+RESET:  LEAS RAMEND
+
+        LDD #$1234
+        STD 10,X
+
+        LEAX 10,X
+        LDY #$1234
+        CMPY ,X
+        BNE BAD
+
+        CLRW ,X
+        CMPY ,X
+        BEQ BAD
+
+        ANDCC #$FB      ; clear Z
+        LDD ,X
+        BNE BAD         ; Z must be set
+
+        include finish.inc
+
+; fill with zeros... up to interrupt table
+        DC.B  [$FFFE-*]0
+        FDB   RESET
+
