5dc497343f43fb810f1d79d7fb137d6abca8dc1a
flor <floryana.gonzalez@jotego.es>
2023-04-05 17:34:19 +0200

    Debugging adc, sbc, and, eor, or idx mode

---
+++ ver/top/tests/and1.asm
@@ -0,0 +1,34 @@
+; Load test
+; 0000-0FFF RAM
+; 1000      Simulation control
+; 1001		read only, A[23:16]
+; F000-FFFF ROM
+
+; Simulation control bits
+; 0 -> set to finish the sim
+; 1 -> set to mark a bad result
+; 5 -> set to trigger IRQ.  Clear manually
+; 6 -> set to trigger FIRQ. Clear manually
+; 7 -> set to trigger NMI.  Clear manually
+
+TESTCTRL EQU $1000
+
+        ORG $F000
+RESET:  LDX #DATAR
+        LDY #05
+
+LOOP:   LDA  ,X
+        ANDA ,X
+        DEC  ,Y
+        CMPA #00
+        BNE LOOP
+
+        include finish.inc
+
+
+DATAR:  FCB 1,2,3,4,5
+
+; fill with zeros... up to interrupt table
+        DC.B  [$FFFE-*]0
+        FDB   RESET
+
---
+++ ver/top/tests/eor1.asm
@@ -0,0 +1,31 @@
+; Load test
+; 0000-0FFF RAM
+; 1000      Simulation control
+; 1001		read only, A[23:16]
+; F000-FFFF ROM
+
+; Simulation control bits
+; 0 -> set to finish the sim
+; 1 -> set to mark a bad result
+; 5 -> set to trigger IRQ.  Clear manually
+; 6 -> set to trigger FIRQ. Clear manually
+; 7 -> set to trigger NMI.  Clear manually
+
+TESTCTRL EQU $1000
+
+        ORG $F000
+RESET:  LDX #$0F
+        LDY #$09
+        EORB ,X
+        EORA ,Y
+        CMPA #$0C
+        BNE BAD
+        CMPB #$7F
+        BNE BAD
+
+        include finish.inc
+
+; fill with zeros... up to interrupt table
+        DC.B  [$FFFE-*]0
+        FDB   RESET
+
