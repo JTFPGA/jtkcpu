1d519ff32dfd8e1b9c5620411ba13cb55097f402
flor <floryana.gonzalez@jotego.es>
2023-04-05 12:20:10 +0200

    subd pass

---
+++ ver/top/tests/subd.asm
@@ -0,0 +1,35 @@
+; Load test
+; 0000-0FFF RAM
+; 1000      Simulation control
+; 1001		read only, A[23:16]
+; F000-FFFF ROM
+
+; Simulation control bits
+; 0 -> set to finish the sim
+; 1 -> set to mark a bad result
+; 5 -> set to trigger IRQ.  Clear manually
+; 6 -> set to trigger FIRQ. Clear manually
+; 7 -> set to trigger NMI.  Clear manually
+
+TESTCTRL EQU $1000
+
+        ORG $F000
+RESET:  LDA  #$0F
+        SUBD #$0A00
+        LDB #$64
+        CMPA #$05
+        BNE BAD
+
+        SUBD #$111
+        CMPD #$453
+        BNE BAD
+
+        STD ,X
+        SUBD ,X
+        CMPD #$00
+        BNE BAD
+
+        include finish.inc
+
+        DC.B  [$FFFE-*]0
+        FDB   RESET
