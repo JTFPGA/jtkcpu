3e1c8bff25f44014548f80e3f0042baa5fa8990b
flor <floryana.gonzalez@jotego.es>
2023-04-06 13:59:09 +0200

    aslw, asrw pass

---
+++ ver/top/tests/asrw.asm
@@ -0,0 +1,61 @@
+; Load test
+; 0000-0FFF RAM
+; 1000      Simulation control
+; 1001		read only, A[23:16]
+; F000-FFFF ROM
+
+; Simulation control bits
+; 0 -> set to finish the sim
+; 1 -> set to mark a bad result
+; 5 -> set to trigger IRQ.  Clear manually
+; 6 -> set to trigger FIRQ. Clear manually
+; 7 -> set to trigger NMI.  Clear manually
+
+RAMEND   EQU $1000
+TESTCTRL EQU $1000
+
+        ORG $F000
+RESET:  LEAS RAMEND
+
+        LDU #$8000
+        STU ,Y
+        ASRW ,Y
+        ASRW ,Y
+        ASRW ,Y
+        ASRW ,Y
+        ASRW ,Y
+        ASRW ,Y
+        ASRW ,Y
+        ASRW ,Y
+        ASRW ,Y
+        ASRW ,Y
+        ASRW ,Y
+        ASRW ,Y
+        LDD ,Y
+        CMPD #$FFF8
+        BNE BAD
+
+        LDD #$4000
+        STD ,X
+        ASRW ,X
+        LDD ,X
+        ASRW ,X
+        LDD ,X
+        ASRW ,X
+        ASRW ,X
+        ASRW ,X
+        LDD ,X
+        CMPD #$0200
+        BNE BAD
+
+        include finish.inc
+
+CHECBK: DC.W $0000
+        DC.W $0001,$0002,$0004,$0008,$0010,$0020,$0040,$0080
+        DC.W $0100,$0200,$0400,$0800,$1000,$2000,$4000,$8000
+CHECKE:
+
+; fill with zeros... up to interrupt table
+        DC.B  [$FFFE-*]0
+        FDB   RESET
+
