f52f3a87f1b81fe1458b5a4e71346168b3a8be3c
flor <floryana.gonzalez@jotego.es>
2023-04-06 11:20:35 +0200

    ror1 pass

---
+++ ver/top/tests/ror1.asm
@@ -0,0 +1,41 @@
+; Load test
+; 0000-0FFF RAM
+; 1000      Simulation control
+; 1001		read only, A[23:16]
+; F000-FFFF ROM
+
+; Simulation control bits
+; 0 -> set to finish the sim
+; 1 -> set to mark a bad result
+; 5 -> set to trigger IRQ.  Clear manually
+; 6 -> set to trigger FIRQ. Clear manually
+; 7 -> set to trigger NMI.  Clear manually
+
+TESTCTRL EQU $1000
+
+        ORG $F000
+RESET:  LDA #$10
+        STA ,X
+        ROR ,X
+        ROR ,X
+        ROR ,X
+        LDA ,X
+        CMPA #$02
+        BNE BAD
+
+        LDB #$04
+        RORB
+        STB ,U
+        ROR ,U
+        ROR ,U
+        ROR ,U
+        LDB ,U
+        CMPB #$80
+        BNE BAD
+
+        include finish.inc
+
+; fill with zeros... up to interrupt table
+        DC.B  [$FFFE-*]0
+        FDB   RESET
+
