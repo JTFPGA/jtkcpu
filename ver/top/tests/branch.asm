9584291ce203419cb16f6148db4d5f516ed43515
flor <floryana.gonzalez@jotego.es>
2023-04-07 09:57:28 +0200

    branch pass

---
+++ ver/top/tests/branch.asm
@@ -0,0 +1,44 @@
+; Load test
+; 0000-0FFF RAM
+; 1000      Simulation control
+; 1001		read only, A[23:16]
+; F000-FFFF ROM
+
+; Simulation control bits
+; 0 -> set to finish the sim
+; 1 -> set to mark a bad result
+; 5 -> set to trigger IRQ.  Clear manually
+; 6 -> set to trigger FIRQ. Clear manually
+; 7 -> set to trigger NMI.  Clear manually
+
+RAMEND   EQU $1000
+TESTCTRL EQU $1000
+
+        ORG $F000
+RESET:
+        LDS #RAMEND
+        LDA #10
+        STA ,X
+        LDB #05
+        CMPB ,X
+        BHI BAD
+
+        CLRD
+        LDA #$0D
+        STA ,X
+        LDB #$23
+        CMPB ,X
+        BLS BAD
+
+        CLRD
+        LDA #$1C
+        STA ,X
+        LDB #$5D
+        CMPB ,X
+        BLT BAD
+
+
+        include finish.inc
+
+        DC.B  [$FFFE-*]0
+        FDB   RESET
