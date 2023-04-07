782921921ede27dbd4e661370f851946658793bd
Jose Tejada <jotego@gmail.com>
2023-04-07 16:32:24 +0200

    bmisc.asm (long branches) PASS

---
+++ ver/top/tests/bmisc.asm
@@ -0,0 +1,40 @@
+; Load test
+; 0000-0FFF RAM
+; 1000      Simulation control
+; 1001		read only, A[23:16]
+; F000-FFFF ROM
+
+; Simulation control bits
+; 0 -> set to finish the sim
+; 1 -> set to mark a bad result
+; 5 -> set to trigger IRQ.  Clear manually
+; 6 -> set to trigger FIRQ. Clear manually
+; 7 -> set to trigger NMI.  Clear manually
+
+RAMEND   EQU $1000
+TESTCTRL EQU $1000
+
+        ORG $F000
+RESET:
+        LDA #$7F
+        CMPA #$50
+        LBGT CONT
+        LBRA BAD
+        DC.B [$110]0
+BGTCK:  LBPL CONT2
+
+        DC.B [$110]0
+CONT:   CMPA #$7F
+        LBNE BAD
+        LBGE BGTCK
+
+        DC.B [$110]0
+CONT2:  LBVS BAD
+        LBVC END
+        LBRA BAD
+
+        DC.B [$110]0
+        include finish.inc
+
+        DC.B  [$FFFE-*]0
+        FDB   RESET
