1f48ef64e61b622480934c994c3a3401a6f9b46f
Jose Tejada <jotego@gmail.com>
2023-04-08 19:32:58 +0200

    tst.asm PASS

---
+++ ver/top/tests/tst.asm
@@ -0,0 +1,43 @@
+; Load test
+; 0000-0FFF RAM
+; 1000      Simulation control
+; 1001		read only, A[23:16]
+; F000-FFFF ROM
+
+; Simulation control bits
+; 0 -> set to finish the sim
+; 1 -> set to mark a bad result
+; 5 -> set to trigger IRQ.  Clear manually
+; 6 -> set to trigger FIRQ. Clear manually
+; 7 -> set to trigger NMI.  Clear manually
+
+TESTCTRL EQU $1000
+
+        ORG $F000
+RESET:  LDA #$80
+        TSTA
+        BEQ BAD
+        CMPA #$80
+        BNE BAD
+
+        LDB #$0
+        TSTB
+        BNE BAD
+        CMPB #$0
+        BNE BAD
+
+        LDA #$3F
+        STA ,X
+        TST ,X
+        BEQ BAD
+
+        LDB ,X
+        CMPB #$3F
+        BNE BAD
+
+        include finish.inc
+
+; fill with zeros... up to interrupt table
+        DC.B  [$FFFE-*]0
+        FDB   RESET
+
